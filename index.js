1c126c35e38e9ae1a7271ef5629ddef25963f895
is-dresko <alejandrotejedor.lpez@gmail.com>
2023-02-20 12:06:05 +0100

    _assocIndexOf

---
+++ index.js
@@ -0,0 +1,10 @@
+const { Client } = require("discord.js");
+const client = new Client({ intents: ["Guilds"] });
+
+client.config = require("./config.json");
+
+client.login(client.config.token).then(() => {
+    console.log(`Cliente iniciado sesión como ${client.user.username}`);
+    client.user.setActivity(`con ${client.guilds.cache.size} servidores.`);
+})
+.catch((err) => console.log(err));
